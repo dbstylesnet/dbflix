900a3ed227feb05a203bdec97a0f9775766237dd
Dariusz <db@dbstyles.net>
2019-11-14 19:03:48 +0000

    Moved data to config file and mapped over it

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import { Wrapper } from './styles'
 import Movie from '../../components/Movie'
-
-import gallery from '../../config/index'
-import avengers from '../../images/avengers.jpeg'
-import avatar from '../../images/avatar.jpg'
-import band from '../../images/band.jpeg'
-import ender from '../../images/ender.jpeg'
-import warcraft from '../../images/warcraft.jpg'
-import xmen from '../../images/xmen.jpg'
+import GalleryData from '../../config/index'
 
 const Gallery = () => {
     return(
-        <>{gallery.movies.avatar.src}
+        <>
             <Wrapper>
-                <Movie imageSrc={avengers} id='avengers' title='Avengers'/>
-                <Movie imageSrc={avatar} id='avatar' title='Avatar' />
-                <Movie imageSrc={band} id='band' title='Band Of Brothers' />
-                <Movie imageSrc={warcraft} id='warcraft' title='Warcraft' />
-                <Movie imageSrc={xmen} id='xmen' title='X-Men' />
-                <Movie imageSrc={ender} id='ender' title={`Ender's Game`} />
+                {GalleryData().map(cover => {
+                    return (
+                        <Movie 
+                            key={cover.id} 
+                            imageSrc={cover.imageSrc} 
+                            id={cover.id} 
+                            title={cover.title} />
+                    );
+                })}
             </Wrapper>
         </>
     )
--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,17 @@
-module.exports = {
-    movies: {
-        avatar: {
-            id: 'avatar',
-            src: '../dupa',
-        }
-    }
-}
+import avengers from '../images/avengers.jpeg'
+import avatar from '../images/avatar.jpg'
+import band from '../images/band.jpeg'
+import ender from '../images/ender.jpeg'
+import warcraft from '../images/warcraft.jpg'
+import xmen from '../images/xmen.jpg'
+
+const GalleryData = () => [
+    {id: 'avengers', imageSrc: avengers, title: 'Avengers'},
+    {id: 'avatar', imageSrc: avatar, title: 'Avatar'},
+    {id: 'band', imageSrc: band, title: 'Band of Brothers'},
+    {id: 'ender', imageSrc: ender, title: 'Ender\'s Game'},
+    {id: 'warcraft', imageSrc: warcraft, title: 'Warcraft'},
+    {id: 'xmen', imageSrc: xmen, title: 'X-Men'},
+]
+
+export default GalleryData
