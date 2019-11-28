90655ee4d32ae3422fd8ee70e8cfdf71aefb6428
Dariusz <db@dbstyles.net>
2019-11-28 08:20:45 +0000

    Removed state from Details page

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,59 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Wrapper } from './styles'
-import Counter from '../../components/Counter'
 import GalleryData from '../../config/index'
 
-class Details extends Component {
-    constructor() {
-        super();
-        this.state = {
-            welcomeMessage: 'Hello, this will be the details page for each Movie & TV show :)',
-            points: 0,
-            movie: {}
-        }
-    }
+const DetailsHooks = (props) => {
 
-    componentDidMount() {
-        let movieId = this.props.match.params.movieId
-        let movie = GalleryData().find((movie) => movie.id === movieId) 
-        this.setState({
-            movie: movie,
-            points: this.state.points + 1000,
-        })
-    }
-
-    runOnClick() {
-        this.setState({
-            welcomeMessage: 'Pressed Up',
-            points: this.state.points + 1,
-        })
-    } 
-
-    runLowClick() {
-        this.setState({
-            welcomeMessage: 'Pressed Down',
-            points: this.state.points - 1,
-        })
-    } 
-
-
-    render() {
-        return(
-            <Wrapper>
-                <div>
-                    Details of the movie:<br />
-                    {this.state.movie.id}<br />
-                    <div> here goes points {this.state.points} </div>
-                    <button onClick={() => this.runOnClick()}>Button here</button><br />
-                    <button onClick={() => this.runLowClick()}>Button low </button><br />
-                    Description goes here. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    <Counter msg={'Hello'} />
-                </div> 
-            </Wrapper>
-        )
-    }
+    let movieId = props.match.params.movieId
+    let movie = GalleryData().find((movie) => movie.id === movieId) 
+ 
+    return (
+        <Wrapper>
+            <div>
+                Details of the movie:<br /><br />
+                {movie.id}<br /><br />
+            </div> 
+        </Wrapper>
+    )
 }
 
-export default Details
+export default DetailsHooks
--- src/pages/kodflix.js
+++ src/pages/kodflix.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AboutUs from '../components/AboutUs'
 import Gallery from '../components/Gallery'
-// import Details from '../components/Details'
-import DetailsHooks from '../components/DetailsHooks'
+import Details from '../components/Details'
+// import DetailsClass from '../components/DetailsClass'
 import { Wrapper } from '../styles/styles'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -15,7 +15,7 @@ const Kodflix = () => {
                 <Header />
                 <Route exact component={Gallery} path='/' />
                 <Route component={AboutUs} path='/about' />
-                <Route component={DetailsHooks} path='/:movieId' />
+                <Route component={Details} path='/:movieId' />
                 <Footer />
             </Router>
         </Wrapper>
